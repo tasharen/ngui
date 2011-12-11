188615f18fbc293d104dcf2a467df0b6714f7889
Michael Lyashenko <michaell@tasharen.com>
2011-12-11 13:15:15 +0000

    - Added tutorial 11 -- checkbox.

---
+++ Assets/NGUI/Scripts/Interaction/UICheckbox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simple checkbox functionality.
+/// </summary>
+
+[AddComponentMenu("NGUI/Interaction/Checkbox")]
+public class UICheckbox : UISend
+{
+	public bool isChecked = true;
+
+	void Start () { Send(isChecked ? 1 : 0); }
+
+	void OnClick ()
+	{
+		isChecked = !isChecked;
+		Send(isChecked ? 1 : 0);
+	}
+}
