766936a790771c87ae459a8c3ea1340b00ce4d29
Michael Lyashenko <michaell@tasharen.com>
2012-02-03 05:38:37 +0000

    - Sound now works on flash, so removing the work-around.

--- Assets/NGUI/Scripts/Internal/NGUITools.cs
+++ Assets/NGUI/Scripts/Internal/NGUITools.cs
@@ -22,12 +22,7 @@ static public class NGUITools
 
 	static public AudioSource PlaySound (AudioClip clip, float volume)
 	{
-#if UNITY_3_4
 		if (clip != null)
-#else
-		// NOTE: There seems to be a bug with PlayOneShot in Flash using Unity 3.5 b6
-		if (clip != null && Application.platform != RuntimePlatform.FlashPlayer)
-#endif
 		{
 			if (mListener == null)
 			{
