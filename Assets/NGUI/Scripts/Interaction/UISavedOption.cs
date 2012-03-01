894b4b31fd15885f5c789bec61b4d7bbb2c2bab4
Michael Lyashenko <michaell@tasharen.com>
2012-03-01 02:46:59 -0500

    - NEW: Added a simple script that can save the state of the checkbox (or a group of checkboxes) to player prefs.

---
+++ Assets/NGUI/Scripts/Interaction/UISavedOption.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+/// <summary>
+/// Attach this script to the parent of a group of checkboxes, or to a checkbox itself to save its state.
+/// </summary>
+
+[AddComponentMenu("NGUI/Interaction/Saved Option")]
+public class UISavedOption : MonoBehaviour
+{
+	/// <summary>
+	/// Load and set the state of the checkboxes.
+	/// </summary>
+
+	void Awake ()
+	{
+		string s = PlayerPrefs.GetString("NGUI State: " + name);
+		
+		if (!string.IsNullOrEmpty(s))
+		{
+			UICheckbox[] checkboxes = GetComponentsInChildren<UICheckbox>();
+
+			foreach (UICheckbox ch in checkboxes)
+			{
+				ch.startsChecked = (ch.name == s);
+				UIEventListener.Add(ch.gameObject).onClick += Save;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Save the state.
+	/// </summary>
+
+	void Save (GameObject go)
+	{
+		UICheckbox[] checkboxes = GetComponentsInChildren<UICheckbox>();
+		
+		foreach (UICheckbox ch in checkboxes)
+		{
+			if (ch.isChecked)
+			{
+				PlayerPrefs.SetString("NGUI State: " + name, ch.name);
+				break;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Save the state on destroy.
+	/// </summary>
+
+	void OnDestroy () { Save(null); }
+}
