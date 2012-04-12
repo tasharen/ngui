06ea1631a5b364bdd48340883e0891ae72dcfc50
Michael Lyashenko <michaell@tasharen.com>
2012-04-12 04:57:37 -0400

    - FIX: UIDraggablePanel won't try to reposition itself if the bounds are empty.

--- Assets/NGUI/Scripts/Interaction/UIDraggablePanel.cs
+++ Assets/NGUI/Scripts/Interaction/UIDraggablePanel.cs
@@ -337,6 +337,7 @@ public class UIDraggablePanel : IgnoreTimeScale
 		DisableSpring();
 
 		Bounds b = bounds;
+		if (b.min.x == b.max.x || b.min.y == b.max.x) return;
 		Vector4 cr = mPanel.clipRange;
 
 		float hx = cr.z * 0.5f;
