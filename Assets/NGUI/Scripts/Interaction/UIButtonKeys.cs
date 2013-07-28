f269458e3293a952ff784b93727d74053236f8e8
Michael Lyashenko <michaell@tasharen.com>
2013-07-28 17:03:40 -0400

    Removed debug logs.

--- Assets/NGUI/Scripts/Interaction/UIButtonKeys.cs
+++ Assets/NGUI/Scripts/Interaction/UIButtonKeys.cs
@@ -27,12 +27,10 @@ public class UIButtonKeys : MonoBehaviour
 		{
 			if (!NGUITools.GetActive(UICamera.selectedObject))
 			{
-				Debug.Log(gameObject.name);
 				UICamera.selectedObject = gameObject;
 			}
 			else
 			{
-				Debug.Log("Hover: " + gameObject.name);
 				UICamera.Notify(gameObject, "OnHover", true);
 			}
 		}
