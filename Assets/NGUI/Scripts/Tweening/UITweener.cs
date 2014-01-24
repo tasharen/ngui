1d610745c2bc16acec79e123e04bdee9842bb42b
Michael Lyashenko <michaell@tasharen.com>
2014-01-24 12:19:20 -0500

    Description update.

--- Assets/NGUI/Scripts/Tweening/UITweener.cs
+++ Assets/NGUI/Scripts/Tweening/UITweener.cs
@@ -360,6 +360,8 @@ public abstract class UITweener : MonoBehaviour
 
 	/// <summary>
 	/// Manually reset the tweener's state to the beginning.
+	/// If the tween is playing forward, this means the tween's start.
+	/// If the tween is playing in reverse, this means the tween's end.
 	/// </summary>
 
 	public void ResetToBeginning ()
