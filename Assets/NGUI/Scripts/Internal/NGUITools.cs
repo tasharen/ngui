7b814732c850b2788079705ca7b6354bda3bdeb0
Michael Lyashenko <michaell@tasharen.com>
2014-06-10 03:35:36 -0400

    FIX: Fix for a regression bug causing bar view foreground's collider was never adjusted properly.

--- Assets/NGUI/Scripts/Internal/NGUITools.cs
+++ Assets/NGUI/Scripts/Internal/NGUITools.cs
@@ -333,9 +333,20 @@ static public class NGUITools
 
 			if (w != null)
 			{
-				Vector3[] corners = w.localCorners;
-				box.center = Vector3.Lerp(corners[0], corners[2], 0.5f);
-				box.size = corners[2] - corners[0];
+				Vector4 dr = w.drawRegion;
+
+				if (dr.x != 0f || dr.y != 0f || dr.z != 1f || dr.w != 1f)
+				{
+					Vector4 region = w.drawingDimensions;
+					box.center = new Vector3((region.x + region.z) * 0.5f, (region.y + region.w) * 0.5f);
+					box.size = new Vector3(region.z - region.x, region.w - region.y);
+				}
+				else
+				{
+					Vector3[] corners = w.localCorners;
+					box.center = Vector3.Lerp(corners[0], corners[2], 0.5f);
+					box.size = corners[2] - corners[0];
+				}
 			}
 			else
 			{
