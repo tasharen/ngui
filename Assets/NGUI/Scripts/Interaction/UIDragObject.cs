fdb49a2326f34ebcd5541ca0ac459d1867498073
Michael Lyashenko <michaell@tasharen.com>
2014-09-28 02:33:17 -0400

    - FIX: UIDragObject will now snap to pixels after the movement completes. - Couple of minor tweaks.

--- Assets/NGUI/Scripts/Interaction/UIDragObject.cs
+++ Assets/NGUI/Scripts/Interaction/UIDragObject.cs
@@ -269,11 +269,11 @@ public class UIDragObject : MonoBehaviour
 		mMomentum -= mScroll;
 		mScroll = NGUIMath.SpringLerp(mScroll, Vector3.zero, 20f, delta);
 
+		// No momentum? Exit.
+		if (mMomentum.magnitude < 0.0001f) return;
+
 		if (!mPressed)
 		{
-			// No momentum? Exit.
-			if (mMomentum.magnitude < 0.0001f) return;
-
 			// Apply the momentum
 			if (panelRegion == null) FindPanel();
 
@@ -289,10 +289,14 @@ public class UIDragObject : MonoBehaviour
 				}
 				else CancelSpring();
 			}
-		}
 
-		// Dampen the momentum
-		NGUIMath.SpringDampen(ref mMomentum, 9f, delta);
+			// Dampen the momentum
+			NGUIMath.SpringDampen(ref mMomentum, 9f, delta);
+
+			// Cancel all movement (and snap to pixels) at the end
+			if (mMomentum.magnitude < 0.0001f) CancelMovement();
+		}
+		else NGUIMath.SpringDampen(ref mMomentum, 9f, delta);
 	}
 
 	/// <summary>
@@ -301,6 +305,14 @@ public class UIDragObject : MonoBehaviour
 
 	public void CancelMovement ()
 	{
+		if (target != null)
+		{
+			Vector3 pos = target.localPosition;
+			pos.x = Mathf.RoundToInt(pos.x);
+			pos.y = Mathf.RoundToInt(pos.y);
+			pos.z = Mathf.RoundToInt(pos.z);
+			target.localPosition = pos;
+		}
 		mTargetPos = (target != null) ? target.position : Vector3.zero;
 		mMomentum = Vector3.zero;
 		mScroll = Vector3.zero;
