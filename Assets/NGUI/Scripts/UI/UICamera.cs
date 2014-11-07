99b1b984fdfe33b8ba7a7ae3ae5bb323fc1d2f53
Michael Lyashenko <michaell@tasharen.com>
2014-11-07 17:38:22 -0500

    Removed unneeded code that was causing problems.

--- Assets/NGUI/Scripts/UI/UICamera.cs
+++ Assets/NGUI/Scripts/UI/UICamera.cs
@@ -1116,18 +1116,12 @@ public class UICamera : MonoBehaviour
 
 	static public void RemoveTouch (int id) { mTouches.Remove(id); }
 
-	[System.NonSerialized] bool mAwakened = false;
-	[System.NonSerialized] bool mEnabled = false;
-
 	/// <summary>
 	/// Add this camera to the list.
 	/// </summary>
 
 	void Awake ()
 	{
-		if (mAwakened) return;
-		mAwakened = true;
-
 		mWidth = Screen.width;
 		mHeight = Screen.height;
 
@@ -1176,19 +1170,15 @@ public class UICamera : MonoBehaviour
 
 	void OnEnable ()
 	{
-		if (!mEnabled)
-		{
-			mEnabled = true;
-			list.Add(this);
-			list.Sort(CompareFunc);
-		}
+		list.Add(this);
+		list.Sort(CompareFunc);
 	}
 
 	/// <summary>
 	/// Remove this camera from the list.
 	/// </summary>
 
-	void OnDisable () { list.Remove(this); mEnabled = false; }
+	void OnDisable () { list.Remove(this); }
 
 	/// <summary>
 	/// We don't want the camera to send out any kind of mouse events.
@@ -1222,12 +1212,7 @@ public class UICamera : MonoBehaviour
 	}
 
 #if UNITY_EDITOR
-	void OnValidate ()
-	{
-		if (!mAwakened) Awake();
-		if (!mEnabled) OnEnable();
-		Start();
-	}
+	void OnValidate () { Start(); }
 #endif
 
 	/// <summary>
